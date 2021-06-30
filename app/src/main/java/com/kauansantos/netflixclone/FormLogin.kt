c712372056625fb204a24c158ebe0e4463a73821
Kauan Santos <kauansantos83@gmail.com>
2021-06-30 19:14:03 -0300

    tela de login finalizada

---
+++ app/src/main/java/com/kauansantos/netflixclone/FormLogin.kt
@@ -0,0 +1,24 @@
+package com.kauansantos.netflixclone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kauansantos.netflixclone.databinding.ActivityFormLoginBinding
+
+class FormLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+        
+        binding.txtTelaCadastro.setOnClickListener {
+            val intent = Intent(this, FormCadastro::class.java)
+           startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/kauansantos/netflixclone/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.kauansantos.netflixclone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({   //SetTimeout do JS
+            AbrirTelaDeLogin()
+        },2000)
+    }
+
+    private fun AbrirTelaDeLogin() {
+        val intent = Intent(this, FormLogin::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
